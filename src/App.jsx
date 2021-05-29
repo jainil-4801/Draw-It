f64d038a3e81044eeecabb7fde10142b1b96c924
jainil-4801 <201851116@iiitvadodara.ac.in>
2021-05-30 00:53:12 +0530

    deployment issue fixing font-awesome library installment

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,9 @@ import {BrowserRouter } from 'react-router-dom';
 import MainComponent from './components/MainComponent';
 import { Provider } from 'react-redux';
 import ConfigurationStore from './Redux/configurationStore';
+
+import 'font-awesome/css/font-awesome.min.css';
+
 const store= ConfigurationStore();
 function App() {
   return (
